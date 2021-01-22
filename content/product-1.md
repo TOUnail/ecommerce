78cdd2f4442a35d90b2acf4ce8e724bbf6095835
atou <atou@marinedepot.com>
2021-01-21 20:21:04 -0800

    static products and map products

---
+++ content/product-1.md
@@ -0,0 +1,11 @@
+---
+name: Product 1
+description: Description of Product 1
+price: 1999
+---
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel convallis nibh. Duis luctus nulla sit amet nibh tempus porta. Nulla eleifend odio a dapibus gravida.
+
+Quisque efficitur non lectus nec pharetra. Vestibulum rutrum, lorem a porta euismod, nibh ex tincidunt eros, in mollis libero erat a dolor. Donec efficitur commodo eros, eu lacinia nunc consectetur ut. Donec id arcu vitae nisi tristique porttitor. Maecenas eleifend tristique libero non hendrerit.
+
+In at bibendum metus. Integer bibendum metus nibh, vel congue sem pulvinar eu. Vestibulum fringilla arcu lacus, vestibulum ultrices purus volutpat sit amet.
--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,35 @@
-const Homepage = () => <div>homepage</div>;
+import Link from "next/link";
+import fs from "fs";
+import matter from "gray-matter";
+const Homepage = (props) => {
+  console.log(props);
+  return props.products.map((product) => (
+    <div>
+      <Link href={product.slug}>
+        <a>{product.name}</a>
+      </Link>
+      <div>{product.description}</div>
+      <div>${product.price / 100}</div>
+    </div>
+  ));
+};
+export const getStaticProps = async () => {
+  const directory = `${process.cwd()}/content`;
+  const filenames = fs.readdirSync(directory);
+  const products = filenames.map((filename) => {
+    const content = fs.readFileSync(`${directory}/${filename}`).toString();
+    const { data } = matter(content);
+    const slug = `/products/${filename.replace(".md", "")}`;
+    const product = {
+      ...data,
+      slug,
+    };
+    return product;
+  });
+  return {
+    props: {
+      products,
+    },
+  };
+};
 export default Homepage;
